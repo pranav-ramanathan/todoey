8af2a4f69df932d1d19709ce51d10e6c5ad6359e
Pranav <pranav.ramanathan@googlemail.com>
2022-12-16 22:17:04 +0000

    Keeps device orientation portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todoey/models/task_data.dart';
 import 'package:todoey/screens/tasks_screen.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,10 @@ var taskData = TaskData();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await taskData.initTaskData();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((value) => runApp(MyApp()));
   runApp(MyApp());
 }
 
